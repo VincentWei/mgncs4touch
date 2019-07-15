8b14339371029d8ad125ea51bc2f7b5352e46ba1
Vincent Wei <vincent@minigui.org>
2019-07-15 14:28:12 +0800

    remove Exception List Statement

--- include/balloon_tip_maker.h
+++ include/balloon_tip_maker.h
@@ -1,3 +1,15 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+//                          IMPORTANT NOTICE
+//
+// The following open source license statement does not apply to any
+// entity in the Exception List published by FMSoft.
+//
+// For more information, please visit:
+//
+// https://www.fmsoft.cn/exception-list
+//
+//////////////////////////////////////////////////////////////////////////////
 /*
  * \file ballon_tip_maker.h
  * \author FMSoft
